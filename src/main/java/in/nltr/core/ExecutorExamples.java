9a73558f97949722f4efcf9e74cf532ebc0b69fb
nltr <learning@inishant.in>
2019-08-03 22:48:45 +0530

    Added executor example

--- src/main/java/in/nltr/core/ExecutorExamples.java
+++ src/main/java/in/nltr/core/ExecutorExamples.java
@@ -3,6 +3,10 @@
  */
 package in.nltr.core;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import java.util.concurrent.Callable;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
@@ -25,6 +29,40 @@ public class ExecutorExamples {
 	 * @param args
 	 */
 	public static void main(String[] args) {
+//		example1();
+		example2();
+	}
+
+	private static void example2() {
+		ExecutorService es=Executors.newFixedThreadPool(3);
+		int i=2;
+		Callable<Integer> f1 =() -> i+1;
+		
+		Callable<Integer> f2=() -> i+2;
+		Callable<Integer> f3 = () -> {try{
+			if(i==2) throw new NullPointerException("111");
+			else
+				return 3;
+		}finally{
+			//do nothing
+		}};
+		List<Callable<Integer>> list=new ArrayList<>();
+		list.add(f1);
+		list.add(f2);
+		list.add(f3);
+		try {
+			List<Future<Integer>> invokeAll = es.invokeAll(list);
+			invokeAll.get(0).get();
+			invokeAll.get(1);
+			invokeAll.get(2).get();
+		} catch (InterruptedException | ExecutionException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		es.shutdown();
+	}
+
+	private static void example1() {
 		ExecutorService es=Executors.newFixedThreadPool(3);
 		int i=2;
 		Future<Integer> f1 = es.submit(() -> i+1);
---
+++ src/main/java/in/nltr/core/IOExamples.java
@@ -0,0 +1,23 @@
+/**
+ * 
+ */
+package in.nltr.core;
+
+import java.io.BufferedReader;
+import java.nio.file.Path;
+
+/**
+ * @author Nishant
+ *
+ */
+public class IOExamples {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+//		Path p= 
+	}
+
+}
